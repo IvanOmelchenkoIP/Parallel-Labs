398bbbf7e939ddc820d3bada0584c7091f5a8134
IvanOmelchenkoIP <io25112002@gmail.com>
2023-02-28 23:31:53 +0200

    Implement Kahan algorithm
    Implement Kahan algorithm into already existing counting for higher
    precision

--- lab1/src/lab1/data/KahanSum.java
+++ lab1/src/lab1/data/KahanSum.java
@@ -3,6 +3,14 @@ package lab1.data;
 public class KahanSum {
 
 	public static double add(double... nums) {
-		return 0;
+		double sum = 0;
+		double err = 0;
+		for (double num : nums) {
+			double y = num - err;
+			double t = sum + y;
+			err = (t - sum) - y;
+			sum = t;
+		}
+		return sum;
 	}
 }
